1a2bfb122de41d6e0377de8af472a3f5adea319e
rafael-circuitstream <rafael@circuitstream.ca>
2026-06-04 20:52:48 -0400

    Class 10

--- Assets/Scripts/PressurePadTrigger.cs
+++ Assets/Scripts/PressurePadTrigger.cs
@@ -3,21 +3,30 @@ using UnityEngine.Events;
 
 public class PressurePadTrigger : MonoBehaviour
 {
+    [SerializeField] private Rigidbody correctRigidbody;
+    [SerializeField] private bool bypassCorrectRigidbody;
+
     public UnityEvent OnPressureActivate;
     public UnityEvent OnPressureDeactive;
 
     protected virtual void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger is activating");
-        OnPressureActivate.Invoke();
+        if (bypassCorrectRigidbody || other.attachedRigidbody == correctRigidbody)
+        {
+            OnPressureActivate.Invoke();
+        }
+
     }
 
     protected virtual void OnTriggerExit(Collider other)
     {
+        if (bypassCorrectRigidbody || other.attachedRigidbody == correctRigidbody)
+        {
+            Debug.Log("Trigger is deactivating");
 
-        Debug.Log("Trigger is deactivating");
+            OnPressureDeactive.Invoke();
+        }
 
-        OnPressureDeactive.Invoke();
     }
 
 }
